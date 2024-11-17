22b5a54b22d4e4e1a2c3363dd5b8db474681c49f
Mateusmourad <mateusmouraduarte@hotmail.com>
2024-11-17 13:39:36 -0300

    spinner select

--- app/src/main/java/com/devspace/taskbeats/CreateOrUpdateTaskBottomSheet.kt
+++ app/src/main/java/com/devspace/taskbeats/CreateOrUpdateTaskBottomSheet.kt
@@ -17,7 +17,7 @@ import com.google.android.material.snackbar.Snackbar
 
 class CreateOrUpdateTaskBottomSheet (
 
-    private val categoryList: List<CategoryUiData>,
+    private val categoryList: List<CategoryEntity>,
     private val task: TaskUiData? = null,
     private val onCreateClicked: (TaskUiData) -> Unit,
     private val onUpdateClicked: (TaskUiData) -> Unit,
@@ -36,9 +36,14 @@ class CreateOrUpdateTaskBottomSheet (
         val btnDelete = view.findViewById<Button>(R.id.btn_task_delete)
         val tieTaskName = view.findViewById<TextInputEditText>(R.id.tie_task_name)
         val spinner: Spinner = view.findViewById(R.id.category_list)
-        var taskCategory: String? = null
 
-        val categoryStr = categoryList.map {it.name}
+        var taskCategory: String? = null
+        val categoryListTemp = mutableListOf("Select")
+        categoryListTemp.addAll(
+            categoryList.map {it.name}
+        )
+
+        val categoryStr = categoryListTemp
 
         ArrayAdapter(
             requireActivity().baseContext,
@@ -95,7 +100,7 @@ class CreateOrUpdateTaskBottomSheet (
 
         btnCreateOrUpdate.setOnClickListener {
             val name = tieTaskName.text.toString().trim()
-            if(taskCategory != null && name.isNotEmpty()) {
+            if(taskCategory != "Select" && name.isNotEmpty()) {
 
                 if (task == null) {
                    // requireNotNull(taskCategory)
